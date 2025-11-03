0c4bb36e3f7694cf50166d9b50848626677cd35f
j-yong98 <chlwodyd9829@gmail.com>
2025-11-03 21:09:03 +0900

    feat: CustomException 추가

---
+++ src/main/java/com/stockbacktest/backend/common/exception/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.stockbacktest.backend.common.exception;
+
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    ;
+    private final HttpStatus status;
+    private final String message;
+    private final LogLevel logLevel;
+
+    ErrorCode(HttpStatus status, String message, LogLevel logLevel) {
+        this.status = status;
+        this.message = message;
+        this.logLevel = logLevel;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
